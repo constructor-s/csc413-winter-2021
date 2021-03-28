6f284a999e785b769077e94e950752b15939834d
B <10003477+constructor-s@users.noreply.github.com>
2021-03-28 19:31:50 -0400

    Add a4 result files

---
+++ a4/gradient_penalty_on/config.py
@@ -0,0 +1,39 @@
+SEED = 11
+
+# Set the random seed manually for reproducibility.
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(SEED)
+
+
+args = AttrDict()
+args_dict = {
+              'image_size':32, 
+              'g_conv_dim':32, 
+              'd_conv_dim':64,
+              'noise_size':100,
+              'num_workers': 0,
+              'train_iters':20000,
+              'X':'Windows',  # options: 'Windows' / 'Apple'
+              'Y': None,
+              'lr':0.00003,
+              'beta1':0.5,
+              'beta2':0.999,
+              'batch_size':32, 
+              'checkpoint_dir': 'results/checkpoints_gan_gp1_lr3e-5',
+              'sample_dir': 'results/samples_gan_gp1_lr3e-5',
+              'load': None,
+              'log_step':200,
+              'sample_every':200,
+              'checkpoint_every':1000,
+              'spectral_norm': False,
+              'gradient_penalty': True,
+              'd_train_iters': 1
+}
+args.update(args_dict)
+
+print_opts(args)
+G, D = train(args)
+
+generate_gif("results/samples_gan_gp1_lr3e-5")
